72b889315c77a87ba08046e468c4bb99d095d1b6
tekkub :bear:  <tekkub@gmail.com>
2011-07-30 04:49:10 -0600

    Nightly backup

--- forums.wowace.com.js
+++ forums.wowace.com.js
@@ -3,9 +3,10 @@
 $("#nav-2nd li:nth-child(2), #nav-2nd li:nth-child(3), #nav-2nd li:nth-child(4)").remove()
 
 // Stick in a mark all as read link
+var markread = $("a[href*='markread']:first").attr("href")
 $("#nav-2nd li:nth-child(3)").before(
   $("<li>").append(
-    $("<a>").attr("href", "http://forums.wowace.com/forumdisplay.php?do=markread").append(
+    $("<a>").attr("href", markread).append(
       $("<span>").text("Mark Forums Read")
     )
   )
