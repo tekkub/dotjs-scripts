cab8bf329d9e3f42b82af755abc895c958d23e48
tekkub :bear:  <tekkub@gmail.com>
2011-10-13 04:25:59 -0600

    Nightly backup

--- github.com.js
+++ github.com.js
@@ -1,3 +1,5 @@
 
 $("body").append("<style> .userbox .notifications_count { display: none !important; } </style>")
 $("body").append("<style>body {zoom: 99%;}</style>")
+$("body").append("<style>.pagehead > ul.tabs li a .counter {font-size: 11px; line-height: 14px;}</style>")
+$("body").append("<style>.subnav-bar ul.subnav .counter {font-size: 11px; padding-top: 2px;}</style>")
