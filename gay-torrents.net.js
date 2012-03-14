91a0c6b7e121577b4634327345ca3d59957ea3a3
tekkub :bear:  <tekkub@gmail.com>
2012-03-14 05:33:47 -0600

    Nightly backup

---
+++ gay-torrents.net.js
@@ -0,0 +1,8 @@
+
+$("ul.torrent-tags").remove()
+$("ul.list_no_decoration > div").first().remove()
+$("#torrents").first().remove()
+$(".torrent-details").first().remove()
+$("#torrent-list-header").first().remove()
+// $("#torrent-list").siblings().remove()
+// $("#torrent-list #torrent-list").siblings().remove()
