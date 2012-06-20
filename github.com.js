744c6e58f50fc655b1f664442f18d335a8030f40
tekkub :bear:  <tekkub@gmail.com>
2012-06-20 05:34:41 -0600

    Nightly backup

--- github.com.js
+++ github.com.js
@@ -4,3 +4,12 @@ $("body").append("<style> #userbox #notifications .unread_count { display: none 
 // $("body").append("<style>body {zoom: 99%;}</style>")
 // $("body").append("<style>.pagehead > ul.tabs li a .counter {font-size: 11px; line-height: 14px;}</style>")
 // $("body").append("<style>.subnav-bar ul.subnav .counter {font-size: 11px; padding-top: 2px;}</style>")
+
+if (document.location == "https://github.com/newsies") {
+  // $("#header,#serverstats,#footer-push,#footer").remove()
+  $("#serverstats,#footer-push,#footer").remove()
+  $(".site").css({padding:0})
+  $("#site-container").css({width:"100%"})
+  $("#newsies").css({height:"100%"})
+  $("#wrapper").css({"margin-bottom":"0px"})
+}
